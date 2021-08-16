4efcb3c294589badc771ca686925d6e33b95391f
Franz <franz.sotoleal@ing.austral.edu.ar>
2021-08-16 19:18:47 -0300

    add navbar and layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from "react";
+import { AppBar, Container, Toolbar, Typography, Box } from "@material-ui/core";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+
 function App() {
-  return <p>App</p>;
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">A2</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container>
+        <Box mt={6}>
+          <HashRouter>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                name="Home Page"
+                render={(props) => <HomePage {...props} />}
+              />
+            </Switch>
+          </HashRouter>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 export default App;
