d555cdeed72b5c89e637627698d085f10cb198d5
Fabrizio Serial <fabrizioserial@sirius.com.ar>
2021-08-25 16:50:49 -0300

    US318 add option button on Project item

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,18 +1,21 @@
-import { useHistory } from "react-router-dom";
+//import { useHistory } from "react-router-dom";
 import {
   Box,
   Card,
   CardContent,
   Chip,
-  Grid,
+  Grid, IconButton,
   Link,
   Typography,
 } from "@material-ui/core";
-import React from "react";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React, {useState} from "react";
+import ItemOptions from "./ItemOptions";
 
 const ProjectItem = (props) => {
   const { item } = props;
-  const history = useHistory();
+  const [anchorEl, setAnchorEl] = useState(false);
+  //const history = useHistory();
 
   const shrinkText = (text) => {
     let nText = text;
@@ -25,40 +28,62 @@ const ProjectItem = (props) => {
     return nText;
   };
 
-  const redirectItem = () => {
+/*  const redirectItem = () => {
     history.push(`/my-projects/${item.id}`);
+  };*/
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   return (
-    <Grid item onClick={redirectItem}>
+    <Grid item >
       <Card variant="outlined">
-        <CardContent>
-          <Box mb={2} fontSize={22} fontWeight={500} color="textPrimary">
-            Project {item.title}
-          </Box>
-          <Grid container spacing={2} direction="row">
-            {item?.tags.map((item, index) => (
-              <Grid item key={index}>
-                <Chip label={item} color="primary" />
-              </Grid>
-            ))}
-          </Grid>
-          <Box my={2}>
-            <Typography color="textSecondary">
-              {shrinkText(item.description)}
-            </Typography>
-          </Box>
-          <Grid container direction="row">
-            <Grid item>
-              <Typography>Link: </Typography>
-            </Grid>
-            <Grid item>
-              <Box ml={0.5}>
-                <Link>link.com.ar</Link>
+          <CardContent>
+            <Grid justifyContent="space-between" container direction="row">
+              <Box mb={2} fontSize={22} fontWeight={500} color="textPrimary">
+                Project {item.title}
+              </Box>
+              <Box>
+                <IconButton
+                    aria-label="more"
+                    aria-controls="long-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                >
+                  <MoreVertIcon />
+                </IconButton>
+                <ItemOptions id={item.id} handleClose={handleClose} anchorEl={anchorEl} />
               </Box>
             </Grid>
-          </Grid>
-        </CardContent>
+
+            <Grid container spacing={2} direction="row">
+              {item?.tags.map((item, index) => (
+                  <Grid item key={index}>
+                    <Chip label={item} color="primary" />
+                  </Grid>
+              ))}
+            </Grid>
+            <Box my={2}>
+              <Typography color="textSecondary">
+                {shrinkText(item.description)}
+              </Typography>
+            </Box>
+            <Grid container direction="row" alignItems="center">
+              <Grid item>
+                <Typography>Link: </Typography>
+              </Grid>
+              <Grid item>
+                <Box ml={0.5}>
+                  <Link>link.com.ar</Link>
+                </Box>
+              </Grid>
+            </Grid>
+          </CardContent>
       </Card>
     </Grid>
   );
