1500195fc5f45b0f141695d8609eccfea60a2cbc
Fabrizio Serial <fabrizioserial@sirius.com.ar>
2021-08-27 12:30:12 -0300

    US314 add delete project

---
+++ src/components/DeleteDialog.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@material-ui/core";
+import {withSnackbar} from "./SnackBarHOC";
+import axios from "axios";
+// eslint-disable-next-line no-undef
+const BASEURL = process.env.REACT_APP_BASEURL;
+
+const DeleteDialog = (props) => {
+    const { open,setOpen, showMessage,id } = props
+
+    const handleClose = () => {
+        setOpen(!open)
+    }
+
+    const handleDelete = async () =>{
+        try {
+            await axios.delete(`${BASEURL}/project/${id}`)
+            showMessage("success","Success to delete project "+id)
+        }catch (e) {
+            showMessage("error","Failed to delete project")
+        }
+    }
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">{"Delete project"}</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Deleting a project is permanent and will remove all content including comments, avatars and profile settings.
+                    Are you sure you want to delete this project?
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={handleDelete} color="primary" autoFocus>
+                    Delete
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default withSnackbar(DeleteDialog);
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -12,10 +12,12 @@ import {
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import React, { useState } from "react";
 import ItemOptions from "./ItemOptions";
+import DeleteDialog from "./DeleteDialog";
 
 const ProjectItem = (props) => {
   const { item } = props;
   const [anchorEl, setAnchorEl] = useState(false);
+  const [open,setOpen] = useState(false);
   //const history = useHistory();
 
   const shrinkText = (text) => {
@@ -42,55 +44,59 @@ const ProjectItem = (props) => {
   };
 
   return (
-    <Grid item>
-      <Card variant="outlined">
-        <CardContent>
-          <Grid justifyContent="space-between" container direction="row">
-            <Box mb={2} fontSize={22} fontWeight={500} color="textPrimary">
-              Project {item.title}
-            </Box>
-            <Box>
-              <IconButton
-                aria-label="more"
-                aria-controls="long-menu"
-                aria-haspopup="true"
-                onClick={handleClick}
-              >
-                <MoreVertIcon />
-              </IconButton>
-              <ItemOptions
-                id={item.id}
-                handleClose={handleClose}
-                anchorEl={anchorEl}
-              />
-            </Box>
-          </Grid>
-
-          <Grid container spacing={2} direction="row">
-            {item?.tags.map((item, index) => (
-              <Grid item key={index}>
-                <Chip label={item} color="primary" />
-              </Grid>
-            ))}
-          </Grid>
-          <Box my={2}>
-            <Typography color="textSecondary">
-              {shrinkText(item.description)}
-            </Typography>
-          </Box>
-          <Grid container direction="row" alignItems="center">
-            <Grid item>
-              <Typography>Link: </Typography>
-            </Grid>
-            <Grid item>
-              <Box ml={0.5}>
-                <Link>link.com.ar</Link>
+    <>
+      <Grid item>
+        <Card variant="outlined">
+          <CardContent>
+            <Grid justifyContent="space-between" container direction="row">
+              <Box mb={2} fontSize={22} fontWeight={500} color="textPrimary">
+                Project {item.title}
+              </Box>
+              <Box>
+                <IconButton
+                  aria-label="more"
+                  aria-controls="long-menu"
+                  aria-haspopup="true"
+                  onClick={handleClick}
+                >
+                  <MoreVertIcon />
+                </IconButton>
+                <ItemOptions
+                  id={item.id}
+                  handleClose={handleClose}
+                  anchorEl={anchorEl}
+                  setDelete={setOpen}
+                />
               </Box>
             </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </Grid>
+
+            <Grid container spacing={2} direction="row">
+              {item?.tags.map((item, index) => (
+                <Grid item key={index}>
+                  <Chip label={item} color="primary" />
+                </Grid>
+              ))}
+            </Grid>
+            <Box my={2}>
+              <Typography color="textSecondary">
+                {shrinkText(item.description)}
+              </Typography>
+            </Box>
+            <Grid container direction="row" alignItems="center">
+              <Grid item>
+                <Typography>Link: </Typography>
+              </Grid>
+              <Grid item>
+                <Box ml={0.5}>
+                  <Link>link.com.ar</Link>
+                </Box>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
+      <DeleteDialog open={open} setOpen={setOpen} id={item.id}/>
+    </>
   );
 };
 export default ProjectItem;
