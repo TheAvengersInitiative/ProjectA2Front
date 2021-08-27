de120dda257d1efa31ecc85c8037ca8adea94c5d
tefiporacchia <36172375+tefiporacchia@users.noreply.github.com>
2021-08-27 11:30:51 -0300

    Alta/Edición de proyecto terminada (#7)
    * alta de proyecto terminada
    
    * Prettier format problem solved
    
    * Delete package-lock.json
    
    * Edit Project done
    
    * Fixed prettier
    
    Co-authored-by: tefiporacchia <stefania.poracchia@ing.austral.edu.ar>

---
+++ src/utils/Projects.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+export const editProject = async (id, data) =>
+  await axiosInstance.put(`/project/${id}`, data);
+export const getProjectById = async (id) =>
+  await axiosInstance.get(`/project/${id}`);
+export const addProject = async (data) =>
+  await axiosInstance.post("/project", data);
