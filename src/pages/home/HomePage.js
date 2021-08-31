d3732246c899bb44849b159840d5d588383c0e9e
fabrizioserial <56046397+fabrizioserial@users.noreply.github.com>
2021-08-30 21:37:41 -0300

    Fix/us 321 (#13)
    * US318 fix - alert dialog closed when render my project
    
    * fix Home page
    
    Co-authored-by: Fabrizio Serial <fabrizioserial@sirius.com.ar>

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ProjectDetail from "../../components/ProjectDetail";
 import { getAllProject } from "../../utils/Projects";
+import { Grid } from "@material-ui/core";
 
 export default function HomePage() {
   const [projects, setProjects] = useState([]);
@@ -19,11 +20,17 @@ export default function HomePage() {
   }, []);
 
   return (
-    <>
+    <Grid container spacing={4} direction="row">
       {projects.length > 0 &&
-        projects.map((item, index) => (
-          <ProjectDetail key={index} project={item} />
-        ))}
-    </>
+        projects.map((item, index) => {
+          return (
+            <Grid item xs key={index}>
+              <Grid container alignItems="center" justifyContent="center">
+                <ProjectDetail project={item} />
+              </Grid>
+            </Grid>
+          );
+        })}
+    </Grid>
   );
 }
