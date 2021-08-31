bb823d50c89f81efa93ef797a438f3aa94d42536
fabrizioserial <56046397+fabrizioserial@users.noreply.github.com>
2021-08-31 19:31:44 -0300

    Fix/316 (#15)
    * US318 fix - alert dialog closed when render my project
    
    * fix edit project fields
    
    * add helpertext
    
    Co-authored-by: Fabrizio Serial <fabrizioserial@sirius.com.ar>
    Co-authored-by: agusayerza <agusayerza@gmail.com>

--- src/components/TextFieldContainer.js
+++ src/components/TextFieldContainer.js
@@ -3,14 +3,14 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 
 export default function TextFieldContainer(props) {
-  const { id, formikProps } = props;
+  const { id, formikProps, helperText } = props;
 
   return (
     <TextField
       id={id}
       name={id}
       fullWidth
-      helperText={formikProps.touched[id] ? formikProps.errors[id] : ""}
+      helperText={formikProps.touched[id] ? formikProps.errors[id] : helperText}
       error={formikProps.touched[id] && Boolean(formikProps.errors[id])}
       value={formikProps.values[id]}
       onChange={formikProps.handleChange}
