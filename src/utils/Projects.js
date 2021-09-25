921c76d1a06576aaf703dc135714db54089a6cb8
tefiporacchia <stefania.poracchia@ing.austral.edu.ar>
2021-09-25 11:42:41 -0300

    just to save

--- src/utils/Projects.js
+++ src/utils/Projects.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8080",
+  headers : {Authorization : "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0QG1haWwuY29tIiwiZXhwIjoxNjMyNTI5NTQ2fQ.IrQWog9BmQfZFMwSaXEIOVqS-I_L8LIXyPgmBOk7Me3puaohKm18tf3Zf6gFfBiUFS31_9SJVfjI4PzCYy4M-w"}
 });
 export const register = async (data) => await axiosInstance.post("/user", data);
 export const editProject = async (id, data) =>
