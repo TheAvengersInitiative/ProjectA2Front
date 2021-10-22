7c60a50c8277c1ba3a49ba4a1e1f66a7c1cbe75f
andresemiliani <56010852+andresemiliani@users.noreply.github.com>
2021-10-22 00:11:57 -0300

    Us 5.1.2 alta de discusión (#34)
    * US318 fix - alert dialog closed when render my project
    
    * US412 add projectDetails page
    
    * US412 update projectDetails page
    
    * US412 add join as collaborator feature
    
    * US412 add successful snackbar
    
    * US412 fix prettier
    
    * US412 add onClick in cards
    
    * US412 add merge with dev
    
    * Project Question
    
    * Alta de discussion, no terminado.
    
    * Improvements and corrections. Still having trouble
    
    * Finished
    
    * Finished Alta de Discusion
    
    * Only Missing Body
    
    * Run prettier. Missing Body.
    
    * Adding a Discussions Counter
    
    * Finished
    
    Co-authored-by: Fabrizio Serial <fabrizioserial@sirius.com.ar>
    Co-authored-by: Andres Emiliani <andresemiliani@ing.austral.edu.ar>

---
+++ src/components/DiscussionsList.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+
+import {
+  Grid,
+  Button,
+  Typography,
+  Box,
+  Chip,
+  Card,
+  CardContent,
+  CardHeader,
+} from "@mui/material";
+import SubmitDialog from "./SubmitDialog";
+
+function DiscussionsList(props) {
+  const { discussions, fetchProject } = props;
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Grid
+        justifyContent="space-between"
+        container
+        direction="row"
+        alignItems="center"
+      >
+        <Typography>Discussions ({discussions.length})</Typography>
+        <Button variant="outlined" disableElevation onClick={handleClickOpen}>
+          Start a discussion
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        {discussions ? (
+          discussions.map((discussion, index) => (
+            <Card variant="outlined" key={index}>
+              <CardHeader title={discussion.title} />
+              <CardContent>
+                <Box ml={1}>
+                  <Grid container direction="row">
+                    <Grid>
+                      <Typography>Tags: </Typography>
+                    </Grid>
+                    <Grid item>
+                      <Grid container direction="row">
+                        {discussion.forumTags.map((item, index) => (
+                          <Box ml={1} key={index}>
+                            <Chip
+                              variant="filled"
+                              color="secondary"
+                              size="small"
+                              label={item.name}
+                            />
+                          </Box>
+                        ))}
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid>
+                    <Typography>Body: {discussion.body} </Typography>
+                  </Grid>
+                  <Typography>
+                    User: {discussion.project.owner.nickname}
+                  </Typography>
+                </Box>
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <Typography>There are no discussions</Typography>
+        )}
+      </Grid>
+      <SubmitDialog
+        open={open}
+        handleClose={handleClose}
+        fetchProject={fetchProject}
+      />
+    </div>
+  );
+}
+
+export default DiscussionsList;
