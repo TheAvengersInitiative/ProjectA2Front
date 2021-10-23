cd59af8139ad991647144e7e95803e039c623c90
Fabrizio Serial <fabrizioserial@sirius.com.ar>
2021-10-22 21:36:09 -0300

    add edit and create comments

--- src/components/DiscussionsList.js
+++ src/components/DiscussionsList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   Grid,
@@ -9,9 +9,19 @@ import {
   Card,
   CardContent,
   CardHeader,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
 } from "@mui/material";
 import SubmitDialog from "./SubmitDialog";
 import styled from "styled-components";
+import { useAuth } from "../contexts/AuthContext";
+import {
+  putCommentDiscussionWithToken,
+  putCommentEditDiscussionWithToken,
+} from "../utils/Projects";
+import { withSnackbar } from "./SnackBarHOC";
 
 const DiscussionContainer = styled.div`
   padding-top: 30px;
@@ -22,10 +32,59 @@ const DiscussionContainer = styled.div`
   padding-bottom: 30px;
 `;
 
+const TextLink = styled.p`
+  color: dodgerblue;
+  cursor: pointer;
+  &:hover {
+    color: royalblue;
+  }
+  width: min-content;
+`;
+
+const CommentContainer = styled(Grid)`
+  padding-left: 50px;
+`;
+
+const CardComment = styled(Card)`
+  margin: 10px 0;
+`;
+
+const DateText = styled.p`
+  font-size: 13px;
+  margin-top: 15px;
+  margin-bottom: 0px;
+  color: lightgrey;
+`;
+
+const AuthorText = styled(DateText)`
+  color: coral;
+`;
+
+const OptionsComment = styled.div`
+  display: flex;
+  flex-direction: row;
+  p {
+    margin-right: 20px;
+    margin-bottom: 0;
+  }
+`;
+
+const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  p {
+    margin-right: 10px;
+  }
+`;
+
 function DiscussionsList(props) {
-  const { discussions, fetchProject } = props;
+  const { discussions, fetchProject, showMessage, user } = props;
+  const { isUserLoggedIn } = useAuth();
 
   const [open, setOpen] = useState(false);
+  const [modalAddComment, setModalAddComment] = useState(false);
+  const [discussionId, setDiscussionId] = useState("");
+  const [defaultText, setDefaultText] = useState("");
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -35,66 +94,224 @@ function DiscussionsList(props) {
     setOpen(false);
   };
 
+  const openModal = (id, discussion, text) => {
+    setDiscussionId(id);
+    discussion && setDefaultText(text);
+    setModalAddComment(!modalAddComment);
+  };
+
   return (
-    <DiscussionContainer>
-      <Grid
-        justifyContent="space-between"
-        container
-        direction="row"
-        alignItems="center"
-      >
-        <Typography>Discussions ({discussions?.length})</Typography>
-        <Button variant="outlined" disableElevation onClick={handleClickOpen}>
-          Start a discussion
-        </Button>
-      </Grid>
-      <Grid item xs={12}>
-        {discussions ? (
-          discussions.map((discussion, index) => (
-            <Card variant="outlined" key={index} style={{ margin: "20px 0" }}>
-              <CardHeader title={discussion.title} />
-              <CardContent>
-                <Box ml={1}>
-                  <Grid container direction="row">
-                    <Grid>
-                      <Typography>Tags: </Typography>
-                    </Grid>
-                    <Grid item>
+    <>
+      <DiscussionContainer>
+        <Grid
+          justifyContent="space-between"
+          container
+          direction="row"
+          alignItems="center"
+        >
+          <Typography>Discussions ({discussions?.length})</Typography>
+          <Button variant="outlined" disableElevation onClick={handleClickOpen}>
+            Start a discussion
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          {discussions ? (
+            discussions.map((discussion, index) => (
+              <>
+                <Card
+                  variant="outlined"
+                  key={index}
+                  style={{ margin: "20px 0" }}
+                >
+                  <CardHeader title={discussion.title} />
+                  <CardContent>
+                    <Box ml={1}>
                       <Grid container direction="row">
-                        {discussion.forumTags.map((item, index) => (
-                          <Box ml={1} key={index}>
-                            <Chip
-                              variant="filled"
-                              color="secondary"
-                              size="small"
-                              label={item.name}
-                            />
-                          </Box>
-                        ))}
+                        <Grid>
+                          <Typography>Tags: </Typography>
+                        </Grid>
+                        <Grid item>
+                          <Grid container direction="row">
+                            {discussion.forumTags.map((item, index) => (
+                              <Box ml={1} key={index}>
+                                <Chip
+                                  variant="filled"
+                                  color="secondary"
+                                  size="small"
+                                  label={item.name}
+                                />
+                              </Box>
+                            ))}
+                          </Grid>
+                        </Grid>
                       </Grid>
-                    </Grid>
-                  </Grid>
-                  <Grid>
-                    <Typography>Body: {discussion.body} </Typography>
-                  </Grid>
-                  <Typography>
-                    User: {discussion.project.owner.nickname}
-                  </Typography>
-                </Box>
-              </CardContent>
-            </Card>
-          ))
-        ) : (
-          <Typography>There are no discussions</Typography>
-        )}
-      </Grid>
-      <SubmitDialog
-        open={open}
-        handleClose={handleClose}
-        fetchProject={fetchProject}
-      />
-    </DiscussionContainer>
+                      <Grid>
+                        <Typography>Body: {discussion.body} </Typography>
+                      </Grid>
+                      <Typography>
+                        User: {discussion.project.owner.nickname}
+                      </Typography>
+                      {isUserLoggedIn() && (
+                        <Grid>
+                          <TextLink onClick={() => openModal(discussion.id)}>
+                            Comment
+                          </TextLink>
+                        </Grid>
+                      )}
+                    </Box>
+                  </CardContent>
+                </Card>
+                <CommentContainer>
+                  {discussion?.comments.map((item, index) => (
+                    <CardComment variant="outlined" key={index}>
+                      <CardContent>
+                        <Grid>
+                          <Grid>{item.comment}</Grid>
+                          <DetailsContainer direction="row">
+                            <DateText>
+                              {`${new Date(item.date).getDate()}/${new Date(
+                                item.date
+                              ).getMonth()}/${new Date(
+                                item.date
+                              ).getFullYear()}`}
+                            </DateText>
+                            <DateText>-</DateText>
+                            <AuthorText>{item.user.nickname}</AuthorText>
+                          </DetailsContainer>
+                          {user && user?.id === item.user.id && (
+                            <OptionsComment>
+                              <TextLink
+                                onClick={() =>
+                                  openModal(item.id, true, item.comment)
+                                }
+                              >
+                                Edit
+                              </TextLink>
+                            </OptionsComment>
+                          )}
+                        </Grid>
+                      </CardContent>
+                    </CardComment>
+                  ))}
+                </CommentContainer>
+              </>
+            ))
+          ) : (
+            <Typography>There are no discussions</Typography>
+          )}
+        </Grid>
+        <SubmitDialog
+          open={open}
+          handleClose={handleClose}
+          fetchProject={fetchProject}
+        />
+      </DiscussionContainer>
+      {modalAddComment && (
+        <AddComment
+          setModalReview={setModalAddComment}
+          fetchProject={fetchProject}
+          id={discussionId}
+          showMessage={showMessage}
+          isUserLoggedIn={isUserLoggedIn}
+          defaultText={defaultText}
+          setDefaultText={setDefaultText}
+        />
+      )}
+    </>
   );
 }
 
-export default DiscussionsList;
+const AddComment = (props) => {
+  const {
+    setModalReview,
+    fetchProject,
+    showMessage,
+    isUserLoggedIn,
+    id,
+    defaultText,
+    setDefaultText,
+  } = props;
+
+  const [text, setText] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setText(defaultText ?? "");
+  }, [defaultText]);
+
+  const addComment = async () => {
+    if (text && text.trim().length < 500 && text.trim().length > 0) {
+      setError(false);
+      try {
+        const body = {
+          comment: text,
+        };
+        if (defaultText) {
+          await putCommentEditDiscussionWithToken(body, id, isUserLoggedIn());
+        } else {
+          await putCommentDiscussionWithToken(body, id, isUserLoggedIn());
+        }
+        showMessage("success", "Review added!");
+        setDefaultText("");
+        fetchProject();
+        setModalReview(false);
+      } catch (e) {
+        showMessage("error", "Oops... Something went wrong!");
+      }
+    } else {
+      setError(true);
+    }
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={() => setModalReview(false)}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      fullWidth={true}
+    >
+      <Box p={1}>
+        <DialogTitle id="alert-dialog-title">{`${
+          defaultText ? "Edit" : "Add"
+        } comment `}</DialogTitle>
+        <DialogContent>
+          <Grid container direction="column" alignItems="center">
+            <Grid item minWidth={530}>
+              <Box mt={2} mb={1}>
+                <TextField
+                  fullWidth={true}
+                  id="outlined-multiline-static"
+                  error={error}
+                  multiline
+                  rows={6}
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  defaultValue="Default Value"
+                  inputProps={{ maxLength: 500 }}
+                  placeholder="Write your comment..."
+                  helperText="Max 500 words"
+                />
+              </Box>
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogContent>
+          <Grid container justifyContent="space-between">
+            <Button
+              onClick={() => (setModalReview(false), setDefaultText(""))}
+              color="error"
+            >
+              Cancel
+            </Button>
+            <Button onClick={() => addComment()} autoFocus>
+              {defaultText ? "Edit" : "Add"}
+            </Button>
+          </Grid>
+        </DialogContent>
+      </Box>
+    </Dialog>
+  );
+};
+
+export default withSnackbar(DiscussionsList);
