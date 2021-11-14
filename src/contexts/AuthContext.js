87024d75066df20c5a7cba14ac89b8656ecce0c0
fabrizioserial <56046397+fabrizioserial@users.noreply.github.com>
2021-11-14 13:46:16 -0300

    Bugfix/delete refresh (#67)
    * US318 fix - alert dialog closed when render my project
    
    * fix refresh
    
    * change history.replace to history.push
    
    * prettier
    
    Co-authored-by: Fabrizio Serial <fabrizioserial@sirius.com.ar>

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, createContext, useEffect } from "react";
+import { getNotification } from "../utils/Projects";
 
 export const AuthContext = createContext();
 
@@ -9,12 +10,7 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState("");
-
-  useEffect(() => {
-    if (localStorage.getItem("token")) {
-      setIsLoggedIn(true);
-    }
-  }, []);
+  const [notification, setNotification] = useState([]);
 
   const getUserInfo = () => isLoggedIn;
 
@@ -33,9 +29,27 @@ export function AuthProvider({ children }) {
     localStorage.setItem("token", response.headers.token);
   };
 
+  const fetchNotification = async () => {
+    try {
+      const response = await getNotification(token);
+      setNotification(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
     setToken(localStorage.getItem("token"));
+    if (localStorage.getItem("token")) {
+      setIsLoggedIn(true);
+    }
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      fetchNotification();
+    }
+  }, [token]);
 
   return (
     <AuthContext.Provider
@@ -46,6 +60,8 @@ export function AuthProvider({ children }) {
         setUserInfo,
         isUserLoggedIn,
         token,
+        notification,
+        fetchNotification,
       }}
     >
       {children}
